8cc33668283f97561cc5f907e6e4e3b3bf15e008
hari prasad sure <hsure@ncsu.edu>
2017-03-08 23:52:53 -0800

    Create Launcher.java

---
+++ Launcher.java
@@ -0,0 +1,20 @@
+import org.apache.commons.collections4.queue;
+import java.util.concurrent.Semaphore;
+
+public class Launcher {
+
+  public static void main(String[] args) {
+
+    int num;
+    CircularFifoQueue<char> queue = new CircularFifoQueue<char>(1024);
+    static Semaphore semaphore = new Semaphore(1);
+
+   Scanner in = new Scanner(System.in);
+   int num = in.nextInt();
+    ReaderThread reader = new ReaderThread(queue,num,mutex);
+    WriterThread writer = new WriterThread(queue,num,mutex);
+
+    new Thread(reader).start();
+    new Thread(writer).start();
+
+  }
